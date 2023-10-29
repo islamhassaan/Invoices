b3a1e04438c86aa662393b46cfbfd6ac80e8be9a
islamhassaan <is2015ha@gmail.com>
2023-10-29 22:02:23 +0200

    Setup Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/{page}', 'AdminController@index');
-
-
 
